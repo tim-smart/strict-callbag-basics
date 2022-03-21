050f014da6280ea9f3992f1e946960372c0cd643
Tim Smart <tim.smart@arisechurch.com>
2022-03-21 16:02:53 +1300

    feat: add run

---
+++ api/run.ts
@@ -0,0 +1,37 @@
+import { Signal, Sink, Source } from "strict-callbag"
+import { createPipe } from "./createPipe"
+
+export const run_ = <A, E, EO>(
+  self: Source<A, E>,
+  sink: Sink<A, E, EO>,
+): Promise<void> =>
+  new Promise((resolve, reject) => {
+    createPipe(self, sink, {
+      onStart(s) {
+        s.pull()
+      },
+      onRequest(s) {
+        s.pull()
+      },
+      onData(_s, data) {
+        sink(Signal.DATA, data)
+      },
+      onEnd(err) {
+        sink(Signal.END, err)
+
+        if (err) {
+          reject(err)
+        } else {
+          resolve()
+        }
+      },
+      onAbort(err) {
+        reject(err)
+      },
+    })
+  })
+
+export const run =
+  <A, E, EO>(sink: Sink<A, E, EO>) =>
+  (self: Source<A, E>) =>
+    run_(self, sink)
