112dc7164b54fd77075cc84965d9296754fdee73
Tim Smart <tim.smart@arisechurch.com>
2022-03-21 13:03:04 +1300

    feat: createPipe and subscribe

---
+++ createPipe.ts
@@ -0,0 +1,59 @@
+import { Signal, Sink, Source, Talkback } from "strict-callbag";
+import { subscribe, Subscription } from "./subscribe";
+
+interface Callbacks<A, EI, EO> {
+  onStart: (sub: Subscription) => void;
+  onData: (sub: Subscription, data: A) => void;
+  onEnd: (err?: EI) => void;
+
+  onRequest: (sub: Subscription) => void;
+  onAbort: (err?: EO) => void;
+
+  talkbackOverride?: (original: Talkback<any>) => Talkback<any>;
+}
+
+/**
+ * Helper for reducing boilerplate when creating operators
+ */
+export const createPipe = <A, EI, EO = never>(
+  source: Source<A, EI>,
+  sink: Sink<any, EI, EO>,
+  {
+    onStart,
+    onData,
+    onEnd,
+
+    onRequest,
+    onAbort,
+
+    talkbackOverride,
+  }: Callbacks<A, EI, EO>
+) => {
+  let sub: Subscription;
+
+  sink(Signal.START, (signal, err) => {
+    if (signal === Signal.DATA) {
+      if (!sub) {
+        sub = subscribe(source, {
+          onStart() {
+            onStart(sub);
+          },
+          onData(data) {
+            onData(sub, data);
+          },
+          onEnd(err) {
+            onEnd(err);
+          },
+          talkbackOverride,
+        });
+
+        sub.listen();
+      } else {
+        onRequest(sub);
+      }
+    } else if (Signal.END) {
+      sub?.cancel();
+      onAbort(err);
+    }
+  });
+};
---
+++ subscribe.ts
@@ -0,0 +1,65 @@
+import { Signal, Source, Talkback } from "strict-callbag";
+
+interface Callbacks<A, E> {
+  onStart: () => void;
+  onData: (data: A) => void;
+  onEnd: (err?: E) => void;
+
+  talkbackOverride?: (original: Talkback<any>) => Talkback<any>;
+}
+
+export const subscribe = <A, E>(
+  source: Source<A, E>,
+  { onStart, onData, onEnd, talkbackOverride }: Callbacks<A, E>
+) => {
+  let aborted = false;
+  let pendingPulls = 0;
+  let talkback: Talkback<any>;
+  let onCancel: (() => void) | undefined;
+
+  const listen = () =>
+    source(Signal.START, (signal, data) => {
+      if (aborted) {
+        if (signal === Signal.START) {
+          data(Signal.END);
+          onCancel?.();
+        }
+        return;
+      }
+
+      if (signal === Signal.START) {
+        talkback = talkbackOverride ? talkbackOverride(data) : data;
+        onStart();
+
+        while (--pendingPulls > 0) {
+          talkback(Signal.DATA);
+        }
+      } else if (signal === Signal.DATA) {
+        onData(data);
+      } else if (signal === Signal.END) {
+        onEnd(data);
+      }
+    });
+
+  const cancel = (cb?: () => void) => {
+    aborted = true;
+    onCancel = cb;
+
+    if (talkback) {
+      talkback(Signal.END);
+      onCancel?.();
+    }
+  };
+
+  const pull = () => {
+    if (talkback) {
+      talkback(Signal.DATA);
+    } else {
+      pendingPulls++;
+    }
+  };
+
+  return { listen, cancel, pull };
+};
+
+export type Subscription = ReturnType<typeof subscribe>;
