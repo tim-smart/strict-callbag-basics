a2f25641ce150635880a3cf43a0a41e165285abb
Tim Smart <tim.smart@arisechurch.com>
2022-03-23 15:45:31 +1300

    chore: add effect-ts to perf

--- perf/run.ts
+++ perf/run.ts
@@ -2,6 +2,9 @@ import { suite, add, cycle, complete } from "benny"
 import * as CB from "../"
 import * as Rx from "rxjs"
 import * as RxO from "rxjs/operators"
+import * as S from "@effect-ts/core/Effect/Experimental/Stream"
+import * as T from "@effect-ts/core/Effect"
+import { pipe } from "@effect-ts/core/Function"
 
 const array = Array.from(Array(100000).keys())
 
@@ -24,6 +27,15 @@ suite(
     const labeled = merged.pipe(RxO.flatMap((i) => Rx.of(`Got: ${i}`)))
 
     await Rx.lastValueFrom(labeled)
+  }),
+  add("@effect-ts/core", async () => {
+    const counts = S.fromIterable(array)
+    const twos = S.filter_(counts, (i) => i % 2 === 0)
+    const threes = S.filter_(counts, (i) => i % 3 === 0)
+    const merged = S.merge_(twos, threes)
+    const labeled = S.chain_(merged, (i) => S.fromIterable([`Got: ${i}`]))
+
+    await T.runPromise(S.runDrain(labeled))
   }),
   cycle(),
   complete(),
