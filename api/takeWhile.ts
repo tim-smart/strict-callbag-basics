3152a977eddcf73b8df65660c61d32170a6a56b3
Tim Smart <tim.smart@arisechurch.com>
2022-03-31 11:07:41 +1300

    refactor: support type narrowing in takeWhile

--- api/takeWhile.ts
+++ api/takeWhile.ts
@@ -1,12 +1,31 @@
 import cbTakeWhile from "callbag-take-while"
 import { Source } from "strict-callbag"
 
-export const takeWhile_ = <A, E>(
+export function takeWhile_<A, E, B extends A>(
   self: Source<A, E>,
-  predicate: (a: A) => boolean,
-): Source<A, E> => cbTakeWhile(predicate)(self as any) as any
+  pred: (a: A) => a is B,
+): Source<B, E>
 
-export const takeWhile =
-  <A>(predicate: (a: A) => boolean) =>
-  <E>(self: Source<A, E>) =>
-    takeWhile_(self, predicate)
+export function takeWhile_<A, E>(
+  self: Source<A, E>,
+  pred: (a: A) => boolean,
+): Source<A, E>
+
+export function takeWhile_<A, E>(
+  self: Source<A, E>,
+  pred: (a: A) => boolean,
+): Source<A, E> {
+  return cbTakeWhile(pred)(self as any) as any
+}
+
+export function takeWhile<A, B extends A>(
+  pred: (a: A) => a is B,
+): <E>(self: Source<A, E>) => Source<B, E>
+
+export function takeWhile<A>(
+  pred: (a: A) => boolean,
+): <E>(self: Source<A, E>) => Source<A, E>
+
+export function takeWhile<A>(pred: (a: A) => boolean) {
+  return <E>(self: Source<A, E>) => takeWhile_(self, pred)
+}
