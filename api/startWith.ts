9e594766906398910c114367008414ff8bc7648a
Tim Smart <tim.smart@arisechurch.com>
2022-03-31 10:02:02 +1300

    fix: use type union for startWith

--- api/startWith.ts
+++ api/startWith.ts
@@ -4,5 +4,5 @@ import StartWith from "callbag-start-with"
 import { Source } from "strict-callbag"
 
 export const startWith = StartWith
-export const startWith_ = <A, E>(self: Source<A, E>, ...as: A[]) =>
-  startWith(...as)(self)
+export const startWith_ = <A, B, E>(self: Source<A, E>, ...bs: B[]) =>
+  startWith(...bs)(self)
--- modules.d.ts
+++ modules.d.ts
@@ -1,7 +1,9 @@
 declare module "callbag-start-with" {
   import { Source } from "strict-callbag"
 
-  const startWith: <A>(...as: A[]) => <E>(source: Source<A, E>) => Source<A, E>
+  const startWith: <B>(
+    ...bs: B[]
+  ) => <A, E>(source: Source<A, E>) => Source<A | B, E>
 
   export default startWith
 }
