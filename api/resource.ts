d438768a826d6f078970251e4330777911565d0b
Tim Smart <tim.smart@arisechurch.com>
2022-04-01 19:57:09 +1300

    feat: add cleanup to resource factory

--- api/resource.ts
+++ api/resource.ts
@@ -5,6 +5,7 @@ import { fromThunk } from "./fromThunk"
 import { NONE } from "./none"
 import { pipe } from "./pipe"
 import { repeatWhile } from "./repeatWhile"
+import { take_ } from "./take"
 
 export const resource =
   <Acc, A, E, E1>(
@@ -13,13 +14,14 @@ export const resource =
       acc: Acc,
       index: number,
     ) => Source<readonly [Acc | NONE, Source<A, E>], E1>,
+    cleanup?: (acc: Acc) => void,
   ): Source<A, E | E1> =>
   (_, sink) => {
     let acc = initial
     let index = 0
 
     pipe(
-      fromThunk(() => project(acc, index++)),
+      fromThunk(() => take_(project(acc, index++), 1)),
       flatten,
       repeatWhile((_, lastItem) => {
         if (lastItem !== NONE && lastItem[0] !== NONE) {
@@ -27,6 +29,7 @@ export const resource =
           return true
         }
 
+        cleanup?.(acc)
         return false
       }),
       chain(([_, source]) => source),
