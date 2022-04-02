789840e1a72197bc395a30fcc8abd83db927e2a9
Tim Smart <tim.smart@arisechurch.com>
2022-04-02 21:10:38 +1300

    refactor: async passes a sink instead of a emitter
    BREAKING CHANGE: AsyncEmitter has been replaced with Sink

--- api/fromEvent.ts
+++ api/fromEvent.ts
@@ -29,20 +29,22 @@ export const fromEvent = <A = unknown>(
   event: string,
   options?: unknown,
 ): Source<A, never> =>
-  async((emit) => {
+  async((sink) => {
+    const onData = (a: A) => sink(1, a)
+
     if ("addListener" in self) {
-      self.addListener(event, emit.data)
+      self.addListener(event, onData)
     } else if ("addEventListener" in self) {
-      self.addEventListener(event, emit.data, options)
+      self.addEventListener(event, onData, options)
     } else {
       throw new Error("fromEvent: not a valid event emitter")
     }
 
     return () => {
       if ("removeListener" in self) {
-        self.removeListener(event, emit.data)
+        self.removeListener(event, onData)
       } else {
-        self.removeEventListener(event, emit.data)
+        self.removeEventListener(event, onData)
       }
     }
   })
