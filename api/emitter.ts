c8ec889afcad3601dd73b2a8f8a66a021b24bdca
Rebecca Stevens <rebecca.stevens@outlook.co.nz>
2022-04-04 12:18:26 +1200

    fix: add "this: void" to methods on interfaces

--- api/emitter.ts
+++ api/emitter.ts
@@ -1,9 +1,9 @@
 import { Signal, Sink } from "strict-callbag"
 
 export interface Emitter<A, E> {
-  data(a: A): void
-  error(e: E): void
-  end(): void
+  data(this: void, a: A): void
+  error(this: void, e: E): void
+  end(this: void): void
 }
 
 export const emitter = <A, E>(sink: Sink<A, E>): Emitter<A, E> => ({
