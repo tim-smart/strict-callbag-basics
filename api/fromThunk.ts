8810c73c880a96c74e7a9de9bc8c0c9562928ed1
Tim Smart <tim.smart@arisechurch.com>
2022-04-07 16:36:42 +1200

    fix: make fromThunk lazy

--- api/fromThunk.ts
+++ api/fromThunk.ts
@@ -4,16 +4,20 @@ export const fromThunk =
   <A>(f: () => A): Source<A, never> =>
   (_, sink) => {
     let unsubed = false
+    let running = false
 
     sink(Signal.START, (signal) => {
-      if (signal === Signal.END) {
+      if (signal === Signal.DATA) {
+        if (!running) {
+          running = true
+          sink(Signal.DATA, f())
+        }
+
+        if (!unsubed) {
+          sink(Signal.END, undefined)
+        }
+      } else if (signal === Signal.END) {
         unsubed = true
       }
     })
-
-    sink(Signal.DATA, f())
-
-    if (!unsubed) {
-      sink(Signal.END, undefined)
-    }
   }
---
+++ test/api/fromThunk.test.ts
@@ -0,0 +1,35 @@
+import { assert } from "chai"
+import { pipe } from "../../index"
+import { describe, test } from "mocha"
+import * as CB from "../../index"
+import { Talkback } from "strict-callbag"
+
+describe("fromThunk", () => {
+  test("it emits the function result", async () => {
+    const result = await pipe(
+      CB.fromThunk(() => 1),
+      CB.toArray,
+      CB.lastItemFrom,
+    )
+
+    assert.deepEqual(result, [1])
+  })
+
+  test("it only emits if asked", async () => {
+    const source = CB.fromThunk(() => 1)
+    const results: number[] = []
+    let tb: Talkback | undefined
+
+    source(0, (signal, data) => {
+      if (signal === 0) {
+        tb = data
+      } else if (signal === 1) {
+        results.push(data)
+      }
+    })
+
+    assert.deepEqual(results, [])
+    tb!(1)
+    assert.deepEqual(results, [1])
+  })
+})
