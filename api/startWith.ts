04b1ec2a12eead2b3b0b17660bf2b75e3c60e9be
Tim Smart <tim.smart@arisechurch.com>
2022-04-11 09:39:53 +1200

    fix: modules reference in startWith

--- api/startWith.ts
+++ api/startWith.ts
@@ -1,6 +1,7 @@
+/// <reference path="../modules.d.ts" />
+
 import StartWith from "callbag-start-with"
 import { Source } from "strict-callbag"
-import "../modules"
 
 export const startWith = StartWith
 export const startWith_ = <A, B, E>(self: Source<A, E>, ...bs: B[]) =>
